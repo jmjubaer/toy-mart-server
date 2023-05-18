d915b6b94c6fb3a57d16e4e5ebd35c4aa3a4b325
jmjubaer <jmjubaer3927@gmail.com>
2023-05-18 23:34:46 +0600

    added addtoy route

--- index.js
+++ index.js
@@ -1,16 +1,54 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wxf5f5n.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const toyCollections = client.db('toy-collection').collection('toy-details');
+
+    app.post('/addToy', async(req,res) => {
+        const data = req.body;
+        const result = await toyCollections.insertOne(data);
+        console.log(data);
+        res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.get('/',(req,res) => {
-    res.send("Jm toy hub server running at http://localhost:5000")
+    res.send("Jm toy mart server running at http://localhost:5000")
 })
 
 
 app.listen(port,() => {
-    console.log("JM toy hub running on port " + port);
+    console.log("JM toy mart running on port " + port);
 });
